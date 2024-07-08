b76e3f079e9a7981636f770436711360b060ccf5
OrukuT <orukutoluwalase@gmail.com>
2024-07-08 02:10:26 +0100

    Done

--- js.js
+++ js.js
@@ -1,120 +1,208 @@
 let userName = prompt("What is your name", " ");
 let user = document.querySelector("#name");
+user.innerHTML = userName;
+
+
+let bod = document.querySelector("body");
+let ptag = document.querySelector("#ptag");
+let ptags = document.querySelector("#ptags");
+let whyChoice = document.querySelector("#Why");
 
 user.innerHTML = userName;
 
+
 if(!(userName === " "))  {
-
-
-let buttonClick = document.querySelector("#button");
-
-
-buttonClicked = buttonClick.addEventListener("click", Round);
-
-
- let Name = document.querySelector("#User");
- let whyChoice = document.querySelector("#Why");
- Name.innerHTML = userName;
-
-let humanScore = 0;
-let computerScore = 0;
-
-let computerReply;
-function Round() {
-function playRound() {
-function getComputerValue()  {
-computerReply = ["Rock", "Paper", "Scissors"];
-computerReply = computerReply[Math.floor(Math.random() * 3)];
-
-}
-getComputerValue();
-       
-let humanReply;
-let r = "rock";
-let p = "paper";
-let s = "scissors";
- let human;
- 
-
-
-
-
-function getBothChoices(humanChoice, computerChoice) {
-  humanReply = prompt("Rock,Paper,Scissors", " ");
-  humanReply = humanReply.toLowerCase();
-      if(humanReply == r || humanReply == p || humanReply == s) {
-       
-   if((humanReply == r && computerReply == "Scissors") || (humanReply == p && computerReply == "Rock") || (humanReply == s && computerReply=="Paper")) {
-   human = "win";
- 
-  } else if((humanReply == p && computerReply == "Scissors") || (humanReply == s && computerReply == "Rock") || (humanReply == r && computerReply=="Paper")) {
-   human = "lose";
-  } else {
-    human = "draw";
-  }
-  
-
-  if(human === "win") {
-   
-    humanScore+=1;
-    let humanUpdate = document.querySelector("#hScore");
-    humanUpdate.innerHTML = humanScore;
-   
-    if(humanReply == r) {
-      whyChoice.innerHTML =  "You won rock beats scissors";
-    } else if(humanReply == p) {
-      whyChoice.innerHTML =  "You won paper beats rock";
+    let buttonClick = document.querySelector("button");
+let sbuttonClick = buttonClick.addEventListener("click", buttonClicked);
+    function buttonClicked() {
+        buttonClick.remove();
+        ptag.textContent = "In order to start the game click one of the buttons below";
+        ptags.textContent = "We would play until someone get up to five points";
       
-    } else {
-      whyChoice.innerHTML =  "You won scissors beats paper";
     }
+    
 
-  }
-
-  if(human === "lose") {
-    computerScore+=1;
-    let computerUpdate = document.querySelector("#cScore");
-    computerUpdate.innerHTML = computerScore;
-  
-  }
-   
-  if(human === "draw") {
-    whyChoice.innerHTML =  "It is a draw";
-  
-
-  }
 
     
+    
+    
+    
+    
+     let Name = document.querySelector("#User");
+    
+     Name.innerHTML = userName;
+    
+    let humanScore = 0;
+    let computerScore = 0;
+    let computerUpdate = document.querySelector("#cScore");
+    let humanUpdate = document.querySelector("#hScore");
+  
+    let human;
+    let computer;
+    
+    let computerReply;
+    
+  
+    
+    
+
+
+
+    
+    let rock =  document.querySelector("#bat");
+    let paper = document.querySelector("#but");
+   
+    let scissors = document.querySelector("#bot");
+    rock = rock.addEventListener("click",  Rock);
+    paper = paper.addEventListener("click",   Paper);
+    scissors = scissors.addEventListener("click",  Scissors)
+    let humanValue; 
+   
+  
+    function Rock() {
+       if(Rock) {
+        computerReply = ["Rock", "Paper", "Scissors"];
+        computerReply = computerReply[Math.floor(Math.random() * 3)];
+        
+       }
+        if(Rock && computerReply == "Scissors") {
+            whyChoice.innerHTML =  "You Won! : Computer chose scissors";
+            human = "Win";
+        } else if(Rock && computerReply == "Paper") {
+            whyChoice.innerHTML =  "You lost! : Computer chose paper";
+           human = "Lose";
+        }  else if(Rock && computerReply == "Rock") {
+            whyChoice.innerHTML =  "It is a draw";
+            human = "Draw";
+        }
+        if(human == "Win") {
+            humanScore+=1;
+            humanUpdate.innerHTML = humanScore;
+            computerUpdate.innerHTML = computerScore;
+        }else if(human == "Lose") {
+            computerScore+=1;
+            humanUpdate.innerHTML = humanScore;
+            computerUpdate.innerHTML = computerScore;
+        } else {
+            humanScore;
+            computerScore;
+            humanUpdate.innerHTML = humanScore;
+            computerUpdate.innerHTML = computerScore;
+        }
+        
+        if(humanScore === 5) {
+            whyChoice.innerHTML =  `${userName} Wins!`;
+            computerScore = 0;
+            humanScore = 0;
+           
+        } else if(computerScore === 5) {
+            whyChoice.innerHTML =  `Computer Wins!`;   
+            computerScore = 0;
+            humanScore = 0;
+        }
+      
+    }
+
+    function Paper() {
+       if(Paper) {
+        computerReply = ["Rock", "Paper", "Scissors"];
+        computerReply = computerReply[Math.floor(Math.random() * 3)];
+       }
+        if(Paper && computerReply == "Rock") {
+            whyChoice.innerHTML =  "You Won! : Computer chose rock";
+            human = "Win";
+
+             } else if(Paper && computerReply == "Scissors") {
+                whyChoice.innerHTML =  "You lost! : Computer chose scissors";
+                human = "Lose";
+     
+             }  else if(Paper && computerReply == "Paper") {
+                whyChoice.innerHTML =  "It is a draw";
+                human = "Draw";
+     
+             }
+
+             if(human == "Win") {
+                humanScore+=1;
+                humanUpdate.innerHTML = humanScore;
+                computerUpdate.innerHTML = computerScore;
+            }else if(human == "Lose") {
+                computerScore+=1;
+                humanUpdate.innerHTML = humanScore;
+                computerUpdate.innerHTML = computerScore;
+            } else {
+                humanScore;
+                computerScore;
+                humanUpdate.innerHTML = humanScore;
+                computerUpdate.innerHTML = computerScore;
+            }
+            
+            
+            if(humanScore === 5) {
+                whyChoice.innerHTML =  `${userName} Wins!`;
+                computerScore = 0;
+                humanScore = 0;
+                
+            } else if(computerScore === 5) {
+                whyChoice.innerHTML =  `Computer Wins!`;   
+                computerScore = 0;
+                humanScore = 0;
+            }
+            
+    }
+
+function Scissors() {
+    if(Scissors) {
+        computerReply = ["Rock", "Paper", "Scissors"];
+        computerReply = computerReply[Math.floor(Math.random() * 3)];
+       }
+        if(Scissors && computerReply == "Paper") {
+            whyChoice.innerHTML =  "You Won! : Computer chose paper";
+            human = "Win";
+             } else if(Scissors && computerReply == "Rock") {
+                whyChoice.innerHTML =  "You lost! : Computer chose rock";
+                human = "Lose";
+     
+             }  else if(Scissors && computerReply == "Scissors") {
+                whyChoice.innerHTML =  "It is a draw";
+                human = "Draw";
+     
+             }
+
+             if(human == "Win") {
+                humanScore+=1;
+                humanUpdate.innerHTML = humanScore;
+                computerUpdate.innerHTML = computerScore;
+            }else if(human == "Lose") {
+                computerScore+=1;
+                humanUpdate.innerHTML = humanScore;
+                computerUpdate.innerHTML = computerScore;
+            } else {
+                humanScore;
+                computerScore;
+                humanUpdate.innerHTML = humanScore;
+                computerUpdate.innerHTML = computerScore;
+            } 
+
+            if(humanScore === 5) {
+                whyChoice.innerHTML =  `${userName} Wins!`;
+                computerScore = 0;
+                humanScore = 0;
+               
+            } else if(computerScore === 5) {
+                whyChoice.innerHTML =  `Computer Wins!`;   
+                computerScore = 0;
+                humanScore = 0;
+            }
+            
+}
   
 
-} else {
-  alert(`"I can't comprehend your Value"`);
-  alert(`Type either "Rock" "Paper" or "Scissors"`);
-} 
-
-}
-getBothChoices();
-}
-playRound();
-playRound();
-playRound();
-playRound();
-playRound();
-
-if(computerScore > humanScore) {
-  whyChoice.innerHTML = "Computer Wins!";
-} else if(computerScore < humanScore) {
-  whyChoice.innerHTML = `"${userName} Wins!"`;
-} else {
-  whyChoice.innerHTML = `"It is a draw"`;
-}
-}
-
-
-}
-  else {
-  alert(`I am sorry, but you cant continue without typing your name`);
-  alert(`Refresh the page if you change your mind`);
-
+ 
  
 }
+   else {
+    whyChoice.textContent = `You must type in your name in order to start`;
+   }
+
+
